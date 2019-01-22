c305f191665bb4587b368202000e012ff2629415
Selwyn <talk@selwyn.cc>
2019-01-22 15:41:42 +0100

    Rewrite links pointing to internal documents
    Closes #9

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,38 @@
 'use strict';
 
 const { readFileSync } = require('fs');
+const markdownItForInline = require('markdown-it-for-inline');
+const path = require('path');
+
+const documentsMetaData = require('../dump.json');
 const generateSidebar = require('../../src/generate-sidebar.js');
 const manifest = require('./public/manifest.json');
+
+const getDocumentFromUrl = url => {
+  const [, urlId] = url.match(/paper\.dropbox\.com\/doc\/.+--\S{26}-(\w{21})/) || [];
+
+  return urlId && documentsMetaData.find(doc => doc.id === urlId) || false;
+};
 
 module.exports = {
   title: 'Playbook',
   themeConfig: {
-    sidebar: generateSidebar(JSON.parse(readFileSync('docs/dump.json'))),
+    sidebar: generateSidebar(documentsMetaData),
+  },
+  extendMarkdown: markdown => {
+    markdown.use(markdownItForInline, 'internal-link', 'link_open', (tokens, index) => {
+      const token = tokens[index];
+      const document = getDocumentFromUrl(token.attrGet('href'));
+
+      if (document) {
+        const location = path.parse(document.location);
+
+        token.attrSet(
+          'href',
+          `/${path.parse(location.dir).name}/${location.name}.html`
+        );
+      }
+    });
   },
   chainMarkdown (config) {
     config.plugin('add-metadata')
